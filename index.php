59f1749749000038d98dce327211860be1af6002
Katarina.Jadinge <katarina.jadinge@gmail.com>
2014-05-08 18:20:48 +0200

    Updated all content

---
+++ index.php
@@ -0,0 +1,9 @@
+<h1>A simple jQuery gallery</h1>
+  <div id="gallery">
+    <img src="gallery/Landscape01.jpg" alt="" id="main-img" width='500px' height='375px'/>
+    <ul>
+      <li><img src="gallery/thumb/Landscape01.jpg" alt="" /></li>
+      <li><img src="gallery/thumb/Landscape02.jpg" alt="" /></li>
+      <li><img src="gallery/thumb/Landscape03.jpg" alt="" /></li>
+    </ul>
+  </div>
---
+++ main.js
@@ -0,0 +1,22 @@
+/**
+ * Gallery with hover function.
+ */
+$(document).ready(function(){
+  'use strict';
+  
+  $("#gallery li img").hover(function(){
+		$('#main-img').attr('src',$(this).attr('src').replace('thumb/', ''));
+	});
+	var imgSwap = [];
+	 $("#gallery li img").each(function(){
+		imgUrl = this.src.replace('thumb/', '');
+		imgSwap.push(imgUrl);
+	});
+	$(imgSwap).preload();
+});
+
+$.fn.preload = function() {
+    this.each(function(){
+        $('<img/>')[0].src = this;
+    });
+};
